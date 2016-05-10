abace4324e5f0fe83f7812fdde0ad3d1c7a70ce0
shamicker <email.shamicker@gmail.com>
2016-05-10 19:01:52 -0400

    Add extra sql for added data

---
+++ populated.sql
@@ -0,0 +1,8 @@
+
+insert into players (player_name) values ('mickey'),
+										 ('minnie'),
+										 ('donald'),
+										 ('daisy');
+insert into matches values (default, '25', '26', '26', '25'),
+						   (default, '27', '28', '28', '27'),
+						   (default, '29', '30', '29', '30');
